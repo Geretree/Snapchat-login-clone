d66f810badb66efa4a50ab2c3d0026c89374b14d
Geretree <sommergeremia@gmail.com>
2026-06-10 11:48:32 +0200

    Remove unused files and update login page structure and styles

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
   
   //script of index.html file
   
-  // Redirect to the main index page after a delay (e.g., 3 seconds)
+  // Redirect to the login page after a delay (e.g., 2.4 seconds)
   setTimeout(function() {
-    window.location.href = "/Acconts.html"; // Redirect to your main index page
-}, 2400); // 3000ms = 3 seconds
+    window.location.href = "/login.html";
+  }, 2400);
 
 
 
